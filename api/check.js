2697139c7793ebdc82be59d3f401a943f74a9e53
MurphyTD1 <murphy1frosty@gmail.com>
2026-07-30 23:55:48 +0300

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,8 +1,5 @@
 export default function handler(req, res) {
-    // Sitemize bağlanan proxy'nin IP adresini yakalıyoruz
     const ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress;
-    
-    // Tarayıcı veya istemci bilgilerini alıyoruz
     const userAgent = req.headers['user-agent'] || "Bilinmiyor";
 
     res.setHeader('Content-Type', 'application/json');
